04f523a81d20b641a60cac48278cb3adb757505a
Davimuler <davidbarba608@gmail.com>
2025-04-18 21:27:25 +0300

    removed unused methods

--- routes/payment.js
+++ routes/payment.js
@@ -4,6 +4,5 @@ const paymentController = require('../controllers/paymentController');
 
 router.post('/create', paymentController.createPayment);
 router.post('/callback', paymentController.handleCallback);
-router.post('/check-status', paymentController.checkProfileStatus);
 
 module.exports = router;
